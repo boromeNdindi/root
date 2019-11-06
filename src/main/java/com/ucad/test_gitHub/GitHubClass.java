d5fd7e4b42b9479f25363ced417ef718c62a6f38
Massamba <massamba@Massambas-MacBook-Pro.local>
2019-11-06 15:22:15 +0000

    my first commit

---
+++ src/main/java/com/ucad/test_gitHub/GitHubClass.java
@@ -0,0 +1,20 @@
+package com.ucad.test_gitHub;
+
+/**
+ * Hello world!
+ *
+ */
+public class GitHubClass 
+{
+   public String getMessage(String nom) {
+	  if(nom.isEmpty())
+		  return "Hello Word "+nom;
+	  else
+	  return "Hello Word ";   
+   }
+   
+   public double sum(double a,double b) {
+	   return a+b;
+   }
+	
+}
